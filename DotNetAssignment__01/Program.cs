3e3a0bf50b882ab9157998c658f6946eeffcac40
Aronno Ahsan <aronno.ahsan.sust@gmail.com>
2023-06-21 21:59:48 +0600

    solved up to Q5

--- DotNetAssignment__01/Program.cs
+++ DotNetAssignment__01/Program.cs
@@ -5,11 +5,11 @@ class Book
 {
 
     //Q2. Implementing appropriate getter and setter methods 
-    private string Title { get; set; }
-    private string Author { get; set; }
-    private string ISBN { get; set; }
-    private int PublicationYear { get; set; }
-    private bool Available { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public int PublicationYear { get; set; }
+    public bool Available { get; set; }
 
     static void Main(string[] args)
     {
@@ -33,5 +33,43 @@ class Book
     //Q3. Creating another class "Library" with list of Book object
 class Library
 {
-    List<Book> books = new List<Book>();
+    public List<Book> books;
+    //Q4. Constructor class that initializes an empty list of books
+    public Library() {
+        books = new List<Book>();
+    }
+
+    //Q5.1. AddBook method takes book object as a parameter and add it to the list of books in the library
+    
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    //Q5.2 RemoveBook takes ISBN as a parameter and removes book that matches the ISBN
+
+    public void RemoveBook(string ISBN)
+    {
+        Book bookToRemove = books.Find(book=>book.ISBN == ISBN);
+        if(bookToRemove != null)
+        {
+            books.Remove(bookToRemove);
+        }
+    }
+
+    //Q5.3 DisplayBooks displays all the books of the Library
+
+    public void DisplayBook()
+    {
+        foreach(Book book in  books)
+        {
+            Console.WriteLine(book.Title);
+            Console.WriteLine(book.Author);
+            Console.WriteLine(book.ISBN);
+            Console.WriteLine(book.PublicationYear);
+            Console.WriteLine(book.Available);
+        }
+    }
+
+
 }
